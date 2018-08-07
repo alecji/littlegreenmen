34dcb974f6a33f27686d40c85e938bd0d2cee628
Daemone <kylebehm@gmail.com>
2018-08-07 11:58:55 -0400

    changes-reverted

--- data.js
+++ data.js
@@ -149,38 +149,28 @@
 //     {
 //         subType: "Pinot Grigio",
 //         type: "Dry White",
-//         description: "pool, summer, fresh"
-//         country: "Italy"
 //     },
 //     {
 //         subType: "Albariño",
 //         type: "Dry White",
-//         description: "dramatic, ocean, sea"
-//         country: "Spain"
+//         description: "mountain, romance, king",
+//         location: "Spain"
 //     },
 //     {
 //         subType: "Gewürztraminer",
 //         type: "Sweet White",
-//         description: "roses, lavish, river"
-//         country: "Gernmany"
 //     },
 //     {
 //         subType: "Müller-Thurgau",
 //         type: "Sweet White",
-//         description: "plain, workers, laborers"
-//         country: "Germany"
 //     },
 //     {
 //         subType: "Malvasia",
 //         type: "Sweet White",
-//         description: "mediterranean, sea, islands"
-//         country: "Greece"
 //     },
 //     {
 //         subType: "Moscato",
 //         type: "Sweet White",
-//         description: "pool, ocean, beach"
-//         country: "Italy"
 //     },
 //     {
 //         subType: "Riesling",
