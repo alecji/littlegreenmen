ac73331be29c530342cec6bf2b9dc9bdacab039b
Your Name <jack.sullivan96@gmail.com>
2018-08-08 10:45:39 -0400

    work on login/signup

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,52 +1,109 @@
 var db = require("../models");
 
-module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-    db.MealPair.findAll({}).then(function(dbMealPairs) {
-      res.render("index");
-    });
-  });
-  
-  app.get("/login", function(req,res) {
-    res.render("login")
-  })
+module.exports = function (app) {
+	// middleware function to check for logged-in users
+	// var sessionChecker = function (req, res, next) {
+	// 	if (req.session.user && req.cookies.user_sid) {
+	// 		res.render("search");
+	// 	} else {
+	// 		next();
+	// 	}
+	// };
 
-  app.get("/signup", function(req,res) {
-    res.render("signup")
-  })
+	// Load index page
+	app.get("/", function (req, res) {
+		res.render("index");
+	});
 
-  // Load results page and display latest match
-  app.get("/search", function(req, res) {
-    // Find most recent history
-    db.History.findAll({
-      raw: true,
-      attributes: ["meal", "winePairings"],
-      limit: 1,
-      order: [ [ 'createdAt', 'DESC']]
-    }).then(function(dbHistoryPairs) {
-      console.log("dbHistoryPairs: " + dbHistoryPairs) ;
-      var rawHistoryPairs = dbHistoryPairs[0] ;
-      console.log("RawhistoryPairs: " + rawHistoryPairs);
-      res.render("search", {
-        historyObject: rawHistoryPairs
-      });
-    });
-  });
+	app.route("/signup")
+		.get( function (req, res) {
+			res.render("signup")
+		})
+		.post(function (req, res) {
+			console.log(req.body)
+			db.User.create({
+				username: req.body.username,
+				email: req.body.email,
+				password: req.body.password
+			})
+				.then(user => {
+					console.log(res.body)
+					req.session.user = user.dataValues;
+					res.redirect("login");
+				})
+				.catch(error => {
+					res.render("signup");
+				});
+		})
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
+	app.route("/login")
+		.get( function (req, res) {
+			res.render("login");
+		})
+		.post(function (req, res) {
+			var username = req.body.username,
+				password = req.body.password;
+			db.User.findOne({
+				where: { username: username }
+			})
+			.then(function (user) {
+				console.log("user: " + user)
+				if (!user) {
+					res.render("login")
+					console.log("Login Failed")
+				} else if (!user.validPassword(password)) {
+					res.render("login")
+					console.log("Login Failed")
+				} else {
+					req.session.user = user.dataValues;
+					res.render("search")
+				}
+			});
+		});
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
+	app.get("/history", function (req, res) {
+		if (req.session.user && req.cookies.user_sid) {
+			res.render("dashboard");
+		} else {
+			res.render("search");
+		}
+	});
+
+	//
+	// Load results page and display latest match
+	app.get("/search", function (req, res) {
+		// Find most recent history
+		db.History.findAll({
+			raw: true,
+			attributes: ["meal", "winePairings"],
+			limit: 1,
+			order: [['createdAt', 'DESC']]
+		}).then(function (dbHistoryPairs) {
+			console.log("dbHistoryPairs: " + dbHistoryPairs);
+			var rawHistoryPairs = dbHistoryPairs[0];
+			console.log("RawhistoryPairs: " + rawHistoryPairs);
+			res.render("search", {
+				historyObject: rawHistoryPairs
+			});
+		});
+	});
+
+	// Load example page and pass in an example by id
+	app.get("/example/:id", function (req, res) {
+		db.Example.findOne({ where: { id: req.params.id } }).then(function (
+			dbExample
+		) {
+			res.render("example", {
+				example: dbExample
+			});
+		});
+	});
+
+	// Render 404 page for any unmatched routes
+	app.get("*", function (req, res) {
+		res.render("404");
+	});
+
+
+
 };
